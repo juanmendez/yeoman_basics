c0330d2b87efdb48e8999bb900b4b2a62bdeaa1d
Juan Mendez <contact@juanmendez.info>
2016-07-07 14:11:25 -0500

    working with extending generator

--- app/index.js
+++ app/index.js
@@ -3,6 +3,11 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
+    constructor: function(){
+      generators.Base.apply( this, arguments );
+
+        this.log( "name:", arguments[0] );
+    },
     method1:function(){
         this.log( "hello world" );
     }
